0f5cac44b42f3507b6609b3073e6cd56061e6508
Jignasha Vanara <jignashavanara@Jignashas-MacBook-Air.local>
2022-04-18 00:54:49 -0400

    front end of project rubric

--- README.md
+++ README.md
@@ -1,12 +1,59 @@
-# Project
+# Pixel Web Company
 
 ## Description
+    Current website is a corporate company portal which is developed using HTML, CSS, JS, Jquery and Bootstrap.
 
 ## Getting Started
 
 ### Folder Structure
+    images - this folder contains all images required for project.
+    js - this folder contains all javascript files.
+    css - this folder contains all css files.
+    home.html - this is a home page of this website.
+    service-docs.html - this page contains employee documents data.
+    service-goal.html - this page is for goal setting of employee's performance check annually.
+    service-time.html - Employee can fill timesheet on daily basis using this page of website.
+    contact - this is a contact page of this website.
+    sign-in.html - this is a login/registration html file.
+    README.TXT - this file contains information regarding project.
 
 ### Executing program
+
+* How to run the program
+Home Page:
+    1. This page contains only information and it's a static page.
+    2. Any user can find company related information on this page.
+
+Service Timesheet Page:
+    1. Logged in employee can fill timesheet on daily basis through this page.
+
+Service Goals Page:
+    1. Logged in employee can add comments of work to achive goal for their performace check annually.
+
+Service Docs Page:
+    1. This page contains only files and it's a static page.
+    2. Logged in employee can see payslip, resume, compensasion letter, offer letter through this page.
+
+Contact Page:
+    1. This page contains only information and it's a static page.
+    2. Any user can find conact infromation thhrough this page.
+
+Login Page:
+    1. on click of login button, system verifies the following validations.
+        - username should not be empty.
+        - password should not be empty and it should have min length of 8 characters.
+    2. on click of sign-up button, user will redirect to register screen.
+
+Registration Page:
+    1. on click of login button, system verifies the following validations.
+        - Firstname should not be empty.
+        - Lastname should not be empty.
+        - DOB should not be empty.
+        - Username should not be empty.
+        - Email should not be empty and it should contain @ in address.
+        - password should not be empty and it should have min length of 8 characters.
+        - Confirm password should not be empty and it should have min length of 8 characters.
+        - Password and Confirm password should have the same values.
 
 ## Help
 (advise for common problems or issues.)
---
+++ js/script.js
@@ -0,0 +1,102 @@
+// Back to top button
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $('.back-to-top').fadeIn('slow');
+  } else {
+    $('.back-to-top').fadeOut('slow');
+  }
+});
+$('.back-to-top').click(function () {
+  $('html, body').animate({ scrollTop: 0 }, 1200, 'easeInOutExpo');
+  return false;
+});
+
+// login-registration 
+function register() {
+  $('#register').css('display', 'flex');
+  $('#login').css('display', 'none');
+}
+
+function login() {
+  $('#register').css('display', 'none');
+  $('#login').css('display', 'flex');
+}
+
+// password validation
+function checkPass() {
+  const pass = document.querySelector('#pass')
+  const confirm_pass = document.querySelector('#confirm-pass')
+  const msg = document.querySelector('.pwd-msg')
+  const btn = document.querySelector('#reg-btn')
+  btn.addEventListener('click', (e) => {
+    if (pass.value != confirm_pass.value) {
+      e.preventDefault();
+      msg.style.display = "block"
+    } else {
+      $('#register').css('display', 'none');
+      $('#login').css('display', 'flex');
+    }
+  })
+}
+
+$(function () {
+  filterObjects('all');
+});
+
+function filterObjects(f) {
+  var item, i;
+  item = document.getElementsByClassName("project-item");
+  if (f == "all") f = "";
+  for (i = 0; i < item.length; i++) {
+    removeClass(item[i], "show");
+    if (item[i].className.indexOf(f) > -1) addClass(item[i], "show");
+  }
+}
+
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Goals 
+var data = Array();
+var i = 0;
+
+function add_comments() {
+  var today = new Date();
+  var date = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = date + ' ' + time;
+  var val = $('#goal-comment').val();
+
+  if (val !== '') {
+    data[i] = $('#goal-comment').val();
+    i++;
+    document.getElementById("goal-comment").value = "";
+
+    var html = "<hr/>";
+    for (var j = 0; j < data.length; j++) {
+      html += "<div class='d-flex flex-row align-items-center commented-user'><h5 class='mr-2'>Employee Name</h5><span class='dot mb-1'></span><span class='mb-1 ml-2'>" + dateTime + "</span></div>" + data[j] + "<br/>";
+    }
+    document.getElementById("goal-data").innerHTML = html;
+
+  }
+}
