b25f3111cbd32f75951b2f3da9ae7c4c328a80ed
Jignasha Vanara <jignashavanara@Jignashas-Air.carolina.rr.com>
2022-04-28 14:24:44 -0400

    authentication update

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs');
+var expressValidator = require('express-validator');
+var flash = require('express-flash');
+var session = require('express-session');
+var mysql = require('mysql');
+var connection  = require('./lib/db');
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -20,6 +26,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({ cookie: { maxAge: 60000 }, 
+  secret: 'pixelweb',
+  resave: false, 
+  saveUninitialized: false}));
+
+app.use(flash());
+app.use(expressValidator());
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
--- routes/routes.js
+++ routes/routes.js
@@ -1,64 +1,150 @@
 var router = require('express').Router();
-// var userData = require('./../src/User.js');
+var userData = require('../src/User.js');
+var connection  = require('../lib/db');
 
 router.get('/', function(req, res, next) {
-    res.render('home');
-});
-
-router.get('/home', function(req, res, next) {
-    res.render('home');
-});
-
-router.get('/api/user', function(req, res, next) {
-    // res.send('user data fetch');
-    const id = Math.random();
-    const firstName = req.query.firstname;
-    const lastName = req.query.lastname;
-    const dob = req.query.birthdate;
-    const userName = req.query.username;
-    const email = req.query.email;
-    const password = req.query.pass;
-
-  res.send({
-    'id': id,
-    'firstName': firstName,
-    'lastName': lastName,
-    'dob': dob,
-    'userName': userName,
-    'email': email,
-    'password': password
-  });
-
-//   userData.User(id, firstName, lastName, dob, userName, email, password);
-
+    if (req.session.loggedin == true) {
+        res.render('home', {
+            layout: 'layout'
+        })
+    } else {
+        res.render('home', {
+            layout: 'loginLayout'
+        })
+    } 
 });
 
 router.get('/about', function(req, res, next) {
-    res.render('home');
+    // res.render('home');
+    if (req.session.loggedin == true) {
+        res.render('home', {
+            layout: 'layout'
+        })
+    } else {
+        res.render('home', {
+            layout: 'loginLayout'
+        })
+    } 
 });
 
 router.get('/annual-performance', function(req, res, next) {
-    res.render('service-goal');
+    // res.render('service-goal');
+    if (req.session.loggedin == true) {
+        res.render('service-goal', {
+            layout: 'layout'
+        })
+    } else {
+        res.render('service-goal', {
+            layout: 'loginLayout'
+        })
+    } 
 });
 
 router.get('/timesheet', function(req, res, next) {
-    res.render('service-time');
+    // res.render('service-time');
+    if (req.session.loggedin == true) {
+        res.render('service-time', {
+            layout: 'layout'
+        })
+    } else {
+        res.render('service-time', {
+            layout: 'loginLayout'
+        })
+    } 
 });
 
 router.get('/documents', function(req, res, next) {
-    res.render('service-docs');
+    // res.render('service-docs');
+    if (req.session.loggedin == true) {
+        res.render('service-docs', {
+            layout: 'layout'
+        })
+    } else {
+        res.render('service-docs', {
+            layout: 'loginLayout'
+        })
+    } 
 });
 
 router.get('/contact', function(req, res, next) {
-    res.render('contact');
+    // res.render('contact');
+    if (req.session.loggedin == true) {
+        res.render('contact', {
+            layout: 'layout'
+        })
+    } else {
+        res.render('contact', {
+            layout: 'loginLayout'
+        })
+    } 
 });
 
 router.get('/login', function(req, res, next) {
-    res.render('sign-in');
+    res.render('sign-in', { layout: 'loginLayout' });
+});
+
+//authenticate user
+router.post('/authentication', function(req, res, next) {
+       
+    var email = req.body.username;
+    var pwd = req.body.pass;
+    console.log(email);
+    console.log(pwd);
+ 
+        connection.query('SELECT * FROM pixelweb_db.users WHERE email = ? AND password = ?', [email, pwd], function(err, rows, fields) {
+            if(err) throw err
+             
+            // if user not found
+            if (rows.length <= 0) {
+                req.flash('error', 'Please enter correct email and Password!');
+                res.redirect('/login');
+            }
+            else { // if user found
+                req.session.loggedin = true;
+                res.redirect('/home');
+ 
+            }            
+        })
+})
+
+//add user
+router.post('/newuser', function(req, res, next) {
+       
+    console.log(req.body);
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var username = req.body.username;
+    var email = req.body.email;
+    var pass = req.body.pass;
+    var confirm_pass = req.body.confirm_pass;
+ 
+        connection.query('INSERT INTO `pixelweb_db`.`users` (`firstname`, `lastname`, `username`, `email`, `password`, `confirm-password`) VALUES ( ?, ?, ?, ?, ?, ?)', [firstname, lastname, username, email, pass, confirm_pass], function(err, rows, fields) {
+            if(err) throw err
+             
+            req.flash('registration', 'User registered successfully.');
+            res.redirect('/login');         
+        })
+})
+
+router.get('/home', function(req, res, next) {
+    if (req.session.loggedin) {
+        res.render('home', {
+            layout: 'layout'
+        })
+    } else {
+        res.render('home', {
+            layout: 'loginLayout'
+        })
+    } 
+});
+
+router.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.redirect('/login');
 });
 
 router.get('/register', function(req, res, next) {
-    res.render('sign-up');
+    res.render('sign-up', { layout: 'loginLayout' });
 });
 
 module.exports = router;
