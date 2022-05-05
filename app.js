adae142355270bd3fd86fe21b4a5ee127423556b
Jignasha Vanara <jignashavanara@Jignashas-Air.carolina.rr.com>
2022-05-05 16:30:51 -0400

    code formatting

--- app.js
+++ app.js
@@ -30,19 +30,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ cookie: { maxAge: 60000 }, 
+app.use(session({
+  cookie: { maxAge: 60000 },
   secret: 'pixelweb',
-  resave: false, 
-  saveUninitialized: false}));
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.use(flash());
 app.use(expressValidator());
 
 //CORS middleware
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");  
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");  
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   next();
 });
 
@@ -54,13 +56,13 @@ app.use('/api/docs', serviceDocsRoutes);
 sql.createTable();
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   // next(createError(404));
   res.redirect('/error').status(404);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
