a75a1c2d91d172a04a234c5e4beaec74815a44e2
JignashaVanara <vanarajignasha159p@gmail.com>
2022-05-09 15:40:52 -0400

    Heroku changes

--- server/models/db.js
+++ server/models/db.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
     user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
 });
 
 connection.connect(function (error) {
